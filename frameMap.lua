597b6769abe73aa4c3fbf2f472528b232cfb206d
andy <andy@andydote.co.uk>
2015-04-02 23:20:13 +0100

    frameMap: turn into an object, calculate all offsets for containers

--- frameMap.lua
+++ frameMap.lua
@@ -1,19 +1,56 @@
 local addon, ns = ...
 
-local map = {}
+local class = ns.lib.class
 
-for bagID = 0, NUM_CONTAINER_FRAMES do
+local frameMap = class:extend({
 
-	map[bagID] = {}
+	ctor = function(self)
+		self.frames = {}
 
-	for slotID = 1, MAX_CONTAINER_ITEMS do
+		self:fillNames()
+	end,
 
-		local name = string.format("ContainerFrame%dItem%d", bagID, slotID)
+	fillNames = function(self)
 
-		map[bagID][slotID] = _G[name]
+		for bagID = 0, NUM_CONTAINER_FRAMES do
 
-	end
+			local bag = {}
 
-end
+			for slotID = 1, MAX_CONTAINER_ITEMS do
 
-ns.frameMap = map
+				local name = string.format("ContainerFrame%dItem%d", bagID, slotID)
+
+				bag[slotID] = _G[name]
+
+			end
+
+			self.frames[bagID] = bag
+
+		end
+
+	end,
+
+	getFrame = function(self, bagID, slotID)
+
+		--MAX_CONTAINER_ITEMS = 36
+		-- bag 0:
+		--  GetContainerNumSlots = 16
+		--  Frame for slotID 1 = 16
+		--  Frame for slotID 2 = 15
+
+		-- bag 1:
+		--  GetContainerNumSlots = 28
+		--  Frame for slotID 1 = 28
+		--  Frame for slotID 2 = 27
+
+		local offset = GetContainerNumSlots(bagID) + 1
+		local framesAvailable = self.frames[bagID]
+
+		return framesAvailable[offset - slotID]
+
+
+	end,
+
+})
+
+ns.frameMap = frameMap
