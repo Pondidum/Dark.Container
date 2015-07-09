94f5714597b7ec2cd4fd0882b8617f77f87b99bf
andy <andy@andydote.co.uk>
2015-07-09 22:20:10 +0100

    run: scratch implementation of containers

--- run.lua
+++ run.lua
@@ -1,16 +1,90 @@
 local addon, ns = ...
 
-local frames = ns.frameMap:new()
-ns.styleBagItems(frames)
 
-local providers = {}
+local layout = Darker.layoutEngine
+local style = Darker.style
 
-for name, provider in pairs(ns.allProviders) do
-	table.insert(providers, provider:new(UIParent))
+
+local run = function()
+
+	local containerIDs = {B}
+
+	for i = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+		table.insert(containerIDs, i)
+	end
+
+	local groups = {}
+
+	local createGroup = function(bagID)
+
+		if groups[bagID] then
+			return groups[bagID]
+		end
+
+		local frame = CreateFrame("Frame", "DarkBagProvider"..bagID, UIParent)
+		frame:SetWidth(390)
+		style:frame(frame)
+
+		local engine = layout:new(frame, {
+				layout = "horizontal",
+				origin = "TOPLEFT",
+				itemSize = 24,
+				itemSpacing = 2,
+				autosize = "y",
+				wrap = true
+		})
+
+		frame.add = function(self, other)
+			engine:addChild(other)
+		end
+
+		frame.layout = function(self)
+			engine:performLayout()
+		end
+
+		groups[bagID] = frame
+
+		return frame
+
+	end
+
+	for i, bagID in ipairs(containerIDs) do
+
+		local bagTotal = GetContainerNumSlots(bagID)
+		for slotID = 1, bagTotal do
+
+			local cellName = string.format("ContainerFrame%dItem%d", bagID+1, bagTotal +1 - slotID)
+			local cell = _G[cellName]
+
+			cell:ClearAllPoints()
+
+			createGroup(bagID):add(cell)
+
+		end
+
+	end
+
+
+
+	for k, f in pairs(groups) do
+
+		if k == 0 then
+			f:SetPoint("CENTER")
+		else
+			f:SetPoint("TOP", groups[k-1], "BOTTOM", 0, -5)
+		end
+
+		f:layout()
+
+	end
+
 end
 
-local containers = { BACKPACK_CONTAINER }
+local runOnce = function()
 
-local scanner = ns.scanner:new(frameMap, providers, containers)
+	run()
+	run =function() end
 
-scanner:layout()
+end
+
+hooksecurefunc("ToggleAllBags", runOnce)
